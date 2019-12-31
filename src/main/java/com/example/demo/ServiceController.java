ea4f691f3df1a0f30ff23936dc9e01656f62e124
Dima <it15111@uom.edu.gr>
2019-12-31 08:01:38 +0200

    added methods to web service Controller , cleaned up some code

--- src/main/java/com/example/demo/ServiceController.java
+++ src/main/java/com/example/demo/ServiceController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,4 +27,25 @@ public class ServiceController {
         return userRepository.save(user);
 
     }
+
+
+    @PostMapping(value = "users/findEmail",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public int findByEmail(@RequestBody Map<String,String> body){
+
+        String email = body.get("email");
+        return userRepository.findAllByEmail(email).size();
+    }
+
+    @PostMapping(value = "users/findUsername",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public int findByUsername(@RequestBody Map<String,String> body){
+
+        String usernmae = body.get("username");
+        return userRepository.findAllByUsername(usernmae).size();
+
+
+    }
+
+
 }
